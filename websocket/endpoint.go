da7d8f3992767efc4f4a8973f05297ec27c84578
Scott Sickles <scottsickles@gmail.com>
2022-08-25 11:29:55 -0400

    added a websocket endpoint helper

---
+++ websocket/endpoint.go
@@ -0,0 +1,26 @@
+package websocket
+
+import (
+	"context"
+	"fmt"
+	"github.com/BSick7/go-api/json"
+	"github.com/nullstone-io/go-streaming/stream"
+)
+
+func Endpoint(w *json.ResponseWriter, r *json.Request, fn func(ctx context.Context, msgs <-chan stream.Message, errs <-chan error)) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	msgs := make(chan stream.Message)
+	defer close(msgs)
+	errs := make(chan error)
+	defer close(errs)
+	broker, err := StartBroker(w, r, msgs, errs)
+	if err != nil {
+		w.SendError(fmt.Errorf("unable to establish websocket connection"))
+		return
+	}
+
+	fn(ctx, msgs, errs)
+
+	broker.WaitForClose()
+}
